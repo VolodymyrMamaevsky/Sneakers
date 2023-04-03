d37dd47c8dd1af25c8ff22a8fc4ec36a12ccdf4e
Volodymyr Mamaevsky <vova.moder@gmail.com>
2023-04-03 18:54:02 +0300

    feat: add macro css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<header>
+				<div className="headerLeft">
+					<img width={40} height={40} src="/img/logo.png" alt="logo" />
+					<div className="headerInfo">
+						<h3>Sneakers</h3>
+						<p>Магазин лучших кроссовок</p>
+					</div>
+				</div>
+				<ul className="headerRight">
+					<li>
+						<img width={18} height={18} src="/img/cart.svg" alt="cart" />
+						<span>1205грн.</span>
+					</li>
+					<li>
+						<img width={18} height={18} src="/img/user.svg" alt="user" />
+					</li>
+				</ul>
+			</header>
+			<div className="content">
+				<h1>Все кроссовки</h1>
+				....
+			</div>
+		</div>
+	);
 }
 
 export default App;
